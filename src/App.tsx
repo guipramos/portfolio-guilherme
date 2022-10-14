9ff6ca08e1a6c27d1414f1a6dd3a818d12dd0709
Guilherme <gui.naris.gr@gmail.com>
2022-10-14 11:06:27 -0300

    Adicionando tagManager

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { Banner } from './components/Banner/Banner';
 import { Content } from './components/Content/Content';
 import { Footer } from './components/Footer/Footer';
 import {Header} from './components/Header/Header';
 import { GlobalStyle } from "./styles/global";
 
+import TagManager from 'react-gtm-module' 
+
 function App() {
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: 'GTM-KSF7C8H'
+    }
+    TagManager.initialize(tagManagerArgs);
+  }, [])
+
   return (
     <div className="App">
       <Header/>
