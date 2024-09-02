2cb50c92d20cbf15714192894868b448318f5d23
Guilherme Ramos <gui.naris.gr@gmail.com>
2024-09-02 19:02:14 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React, { useEffect} from 'react';
-import { Banner } from './components/Banner/Banner';
-import { Content } from './components/Content/Content';
-import { Footer } from './components/Footer/Footer';
-import {Header} from './components/Header/Header';
+import React, { useEffect } from "react";
+import { Banner } from "./components/Banner/Banner";
+import { Content } from "./components/Content/Content";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
 import { GlobalStyle } from "./styles/global";
 
-import TagManager from 'react-gtm-module' 
+import TagManager from "react-gtm-module";
 
 function App() {
   useEffect(() => {
     const tagManagerArgs = {
-      gtmId: 'GTM-KSF7C8H'
-    }
+      gtmId: "GTM-KSF7C8H",
+    };
     TagManager.initialize(tagManagerArgs);
-  }, [])
+  }, []);
 
   return (
     <div className="App">
@@ -21,10 +21,10 @@ function App() {
       <Banner />
       <Content />
       <Footer /> */}
-      <div className='manutencao'>
+      <div className="manutencao">
         <span>Pagina em construção</span>
       </div>
-      <GlobalStyle/>
+      <GlobalStyle />
     </div>
   );
 }
